6c6b5bcaefedede4c7e6486086c5a70b4dfdd8f9
Adarsha Halder <pradiphalder70@gmail.com>
2024-08-27 09:58:16 +0530

    gh

--- src/components/pages/InAppForm (Edit Form)/caseStudy.js
+++ src/components/pages/InAppForm (Edit Form)/caseStudy.js
@@ -3,17 +3,18 @@ import React from "react";
 const Case = ({ formData, handleChange }) => {
   return (
     <>
+      <div className="MarketSize-Inapp1">
       <div className="mb-4">
         <label
           htmlFor="caseStudies"
-          className="block text-[3vw] md:text-[3vw] md:text-[1.2vw] font-medium text-white"
+          className="block text-[3vw] md:text-[1.2vw] font-medium text-white"
         >
           Could you provide examples of successful case studies from your
           clients, showcasing how your products or services have made a positive
           impact on their businesses? *
         </label>
         <textarea
-          className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 bg-transparent text-[3vw] md:text-[3vw] md:text-[1.2vw]"
+          className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 bg-transparent text-[3vw] md:text-[1.2vw]"
           id="caseStudies"
           name="caseStudies"
           value={formData.caseStudies}
@@ -21,6 +22,7 @@ const Case = ({ formData, handleChange }) => {
           rows={6}
           required
         />
+      </div>
       </div>
     </>
   );
